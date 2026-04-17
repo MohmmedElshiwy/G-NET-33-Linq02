9b3d477b8d7a93e11a231b57139178fc307dbfba
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-04-17 16:57:49 -0700

    Solve the problem problem 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.DataSources;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var products = Source.ProductList;
+
+            #region Get top 3 most expensive products
+
+
+            var result = products.OrderByDescending(p => p.UnitPrice).Take(3);
+            foreach (var item in result) { Console.WriteLine($"Name:{item.ProductName}  |    Price: {item.UnitPrice}q"); }
+            #endregion
         }
     }
 }
