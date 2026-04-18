269570c0a53c73349879acf4aa7014d383abdb6d
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-04-17 17:24:01 -0700

    Solve the problem problem 2

--- Program.cs
+++ Program.cs
@@ -11,9 +11,18 @@ namespace ConsoleApp1
             #region Get top 3 most expensive products
 
 
-            var result = products.OrderByDescending(p => p.UnitPrice).Take(3);
-            foreach (var item in result) { Console.WriteLine($"Name:{item.ProductName}  |    Price: {item.UnitPrice}q"); }
+            //var result = products.OrderByDescending(p => p.UnitPrice).Take(3);
+            //foreach (var item in result) { Console.WriteLine($"Name:{item.ProductName}  |    Price: {item.UnitPrice}q"); }
             #endregion
+
+            #region page 2 of products, with page size = 5
+
+            //int PageSize = 5;
+            //int PageNumber = 2;
+            //var result = products.OrderBy(p => p.ProductID).Skip((PageNumber - 1) * PageSize).Take(PageSize);
+            //foreach (var item in result) { Console.WriteLine($" ID : {item.ProductID}   |   Name : {item.ProductName}"); }
+            #endregion
+
         }
     }
 }
